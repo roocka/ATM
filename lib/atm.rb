4751fe41eb463c2811db4402292939dee7e89258
you name <your@email.com>
2017-06-22 12:15:18 +0200

    updatind lib/atm and spec/atm

--- lib/atm.rb
+++ lib/atm.rb
@@ -4,4 +4,13 @@ class Atm
   def initialize
     @funds = 1000
   end
+
+  def withdraw(amount)
+    @funds -= amount
+  end
+end
+
+class Account()
+
+
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,6 +1,20 @@
 require './lib/atm.rb'
 describe Atm do
-  it 'has 1000$ on initialize' do
-    expect(subject.funds).to eq 1000
+let (:account) {instance_double('Account')}
+
+before do
+allow(account).to receive(:balance).and_return(100)
+allow(account).to receive(:balance=)
+end
+
+
+
+  it 'has $1000 on initialize' do
+    expect(subject.funds) .to eq 1000
+  end
+
+  it 'funds are reduced at withdraw' do
+    subject.withdraw 50
+    expect(subject.funds).to eq 950
   end
 end
